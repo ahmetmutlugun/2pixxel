74b5e6e5bb9637aa6f296c22d35ad8e5f81553d9
Ahmet <35678867+ahmetmutlugun@users.noreply.github.com>
2022-10-20 00:00:27 -0700

    Initial Commit

---
+++ scale2x/scale2xApp.swift
@@ -0,0 +1,17 @@
+//
+//  scale2xApp.swift
+//  scale2x
+//
+//  Created by Ahmet on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct scale2xApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
